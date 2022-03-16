e8f16cbf4242e7c6b597831b57055666e156b501
tanshi2003 <100862323+tanshi2003@users.noreply.github.com>
2022-03-16 12:25:28 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,11 @@
+# python program to print positive numbers in given range
+
+start, end = -4, 10
+
+#iterating each number in list 
+for num in range(start, end + 1):
+     
+    #checking condition
+    if num>= 0:
+        print (num,end ="")
+        
